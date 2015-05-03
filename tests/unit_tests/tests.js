3ee5d42962fba3524c53c99440980a528e89e160
zikicm <zikicm@gmail.com>
2015-05-03 17:43:40 +0200

    Define.js unit tests: 1) Basic define / require test 2) Exception on requirement of undefined module

--- tests/unit_tests/tests.js
+++ tests/unit_tests/tests.js
@@ -1,3 +1,41 @@
-QUnit.test( "Hello test", function( assert ) {
+var test = QUnit.test;
+
+test("Hello test", function(assert) {
   assert.ok( 1 === 1, "Passed!" );
+});
+
+// Define.js
+
+test("Define.js: Module define / require", function(assert) {
+
+	define("someLib/testModuleA", function(require, exports, module) {
+
+		module.exports = function() {
+			return 1;
+		}
+
+	});
+
+	define("otherLib/testModuleB", function(require, exports, module) {
+
+		var testFunc = require("someLib/testModuleA");
+		assert.ok(1 === testFunc(), "Passed!");
+
+	});
+
+});
+
+test("Define.js: Exception on require of undefined module", function(assert) {
+
+	var moduleName = "someLib/testModuleC";
+	var moduleFunc = function(require, exports, module) {
+		require("someLib/someUndefinedModule");
+	}
+	var moduleDefine = define.bind(null, moduleName, moduleFunc);
+
+	assert.throws(
+		moduleDefine, // Block which should throw exception
+		new Error("Module someLib/someUndefinedModule not defined!"), // Expected exception
+		"Passed!");
+
 });
